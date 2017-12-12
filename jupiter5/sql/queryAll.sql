c6e03fb1198ff4156fa8b1f86f60b2d9609ce99f
risonakamo <hmykanae@gmail.com>
2017-12-12 13:32:39 -0500

    +writing fixed up queries

--- jupiter5/sql/queryAll.sql
+++ jupiter5/sql/queryAll.sql
@@ -5,21 +5,21 @@ select distinct last_name,first_name from applicants,application where degree_na
 select count(student_id),degree_name,semester,year from application group by degree_name,semester,year;
 
 /* query 3 */
-select count(major) as majorcount,major from education group by major order by majorcount desc limit 1;
+select count(major) majorcount,major from education where year(grad_date)="2015" group by major order by majorcount desc limit 1;
 
 /* query 4 */
 select last_name,first_name,gpa from applicants,
 (select student_id,gpa
 from application,
 (select application_id,gpa from education where gpa=(select min(gpa) from education)) as mingpa
-where application.application_id=mingpa.application_id and application.decision="accepted") as mingpa where
+where application.application_id=mingpa.application_id and application.decision="accepted" and year(application.decision_date)=year(curdate()) and semester="spring") as mingpa where
 applicants.student_id=mingpa.student_id;
 
 /* query 5 */
 select count(application.application_id),degree_name,major from application,education where application.application_id=education.application_id group by degree_name,major;
 
 /* query 6 */
-select * from application where eval_date<"2000-01-01";
+select application_id from application where eval_date<"2000-01-01";
 
 /* query 7 */
 select count(application_id),decision,degree_name,year from application group by decision,degree_name,year;
@@ -35,5 +35,5 @@ where acceptenceCount.acceptences=maxacceptences.maxacceptences;
 /* query 10 */
 select collegecountmax.collegecountmax,college from
 (select count(application_id) as collegecount,college from education where year(grad_date)>year(curdate())-5 group by college) as collegecount,
-(select max(collegecount.collegecount) as collegecountmax from (select count(application_id) collegecount,college from education where grad_date>"2014-01-01" group by college) as collegecount) as collegecountmax
+(select max(collegecount.collegecount) as collegecountmax from (select count(application_id) collegecount,college from education where year(grad_date)>year(curdate())-5 group by college) as collegecount) as collegecountmax
 where collegecount.collegecount=collegecountmax.collegecountmax;
